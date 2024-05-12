46243d5790856460362616854b8d71f2dea4b2f9
kimmsan <kimsan.technology@gmail.com>
2024-05-12 12:52:44 +0700

    first and completed commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        <b>Version</b> 1.1
+    </div>
+    <strong>Copyright &copy; {{ date('Y') }} <a href="/"> {{ $setting->nama_perusahaan }} </a>  Development by <a href="https://t.me/kimm_san"> MR.KIMSAN.</a></strong> All rights
+    reserved.
+</footer>
